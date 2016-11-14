7b7d3d54ee730c1bae80fba637b6493a8bf22463
James Tomlinson <tomo.bbe@gmail.com>
2016-11-14 17:46:49 +0000

    Refactored functions to return log-logistic fit parameters. Separate function to calculate function parameters only.

--- pyspei/_spei.pyx
+++ pyspei/_spei.pyx
@@ -3,7 +3,21 @@ cimport spei as c_spei
 import numpy as np
 cimport numpy as np
 
-cpdef spei(float[:] balanceSeries, int months, int seasons, float[:] speiSeries):
+
+cdef float[:] acummulate_series(float[:] balanceSeries, int months):
+    cdef int n = balanceSeries.shape[0]
+    cdef int i, j
+	# Compute the cumulative series
+    cdef int acumN = n-months+1
+    cdef float[:] acumSeries = np.zeros(acumN, dtype=np.float32)
+    for i in range(months-1, n):
+        for j in range(months):
+            acumSeries[i-months+1] += balanceSeries[i-j]
+
+    return acumSeries
+
+cpdef spei(float[:] balanceSeries, int months, int seasons, float[:] speiSeries,
+           float[:] beta=None, float[:, :] logLogisticParams=None):
     """
     Calculate SPEI for a monthly series of effective rainfall
 
@@ -23,28 +37,42 @@ cpdef spei(float[:] balanceSeries, int months, int seasons, float[:] speiSeries)
     None
 
     """
-    cdef int n = balanceSeries.shape[0]
-    cdef int i, j
+    cdef float[:] acumSeries = acummulate_series(balanceSeries, months)
+    cdef int acumN = acumSeries.shape[0]
 
-	# Compute the cumulative series
-    cdef int acumN = n-months+1
-    cdef float[:] acumSeries = np.zeros(acumN, dtype=np.float32)
-    for i in range(months-1, n):
-        for j in range(months):
-            acumSeries[i-months+1] += balanceSeries[i-j]
+    if beta is None and logLogisticParams is None:
+        beta = np.empty(3, dtype=np.float32)
+        # NB the 1 based indexing.
+        logLogisticParams = np.empty((seasons+1, 3), dtype=np.float32)
+        # Calculate log-logistic distribution parameters
+        _fit_loglogistic(acumSeries, seasons, beta, logLogisticParams)
+
     # Calculate SPEI
-    #c_spei.spei(&acumSeries[0], acumN, seasons, &speiSeries[0])
-    _spei(acumSeries, acumN, seasons, speiSeries)
+    _spei(acumSeries,  seasons,  beta, logLogisticParams, speiSeries)
+    return beta, logLogisticParams
 
+
+def fit_loglogistic(float[:] balanceSeries, int months, int seasons):
+    cdef float[:] acumSeries = acummulate_series(balanceSeries, months)
+    cdef int acumN = acumSeries.shape[0]
+
+    cdef float[:] beta = np.empty(3, dtype=np.float32)
+    # NB the 1 based indexing.
+    cdef float[:, :] logLogisticParams = np.empty((seasons+1, 3), dtype=np.float32)
+
+    # Calculate log-logistic distribution parameters
+    _fit_loglogistic(acumSeries, seasons, beta, logLogisticParams)
+    return beta, logLogisticParams
 
 cpdef thornthwaite(float[:] tempSeries, float lat, float[:] etpSeries):
 
     cdef int n = tempSeries.shape[0]
     c_spei.thornthwaite(&tempSeries[0], n, lat, &etpSeries[0])
 
-cdef _spei(float[:] dataSeries, int n, int seasons, float[:] speiSeries):
+
+cdef _fit_loglogistic(float[:] dataSeries, int seasons, float[:] beta, float[:, :] logLogisticParams):
     """
-    Python port of C spei() function.
+    Python port of first half of C spei() function.
 
     Calculates the Standardized Precipitation-Evapotransporation Index
      from a series of climatic balance (precipitation minus etp). The
@@ -53,11 +81,9 @@ cdef _spei(float[:] dataSeries, int n, int seasons, float[:] speiSeries):
 
     """
     cdef int i, j, k
+    cdef int n = dataSeries.shape[0]
     cdef int nSeason = n // seasons + 1
     cdef float[:] seasonSeries = np.empty(nSeason, dtype=np.float32)
-    cdef float[:] beta = np.empty(3, dtype=np.float32)
-    # NB the 1 based indexing.
-    cdef float[:, :] logLogisticParams = np.empty((seasons+1, 3), dtype=np.float32)
 
 	# Loop through all seasons defined by seasons
     for j in range(1, seasons+1):
@@ -74,6 +100,21 @@ cdef _spei(float[:] dataSeries, int n, int seasons, float[:] speiSeries):
         # Fit a Log Logistic probability function
         c_spei.logLogisticFit(&beta[0], &logLogisticParams[j, 0])
 
+
+cdef _spei(float[:] dataSeries, int seasons, float[:] beta, float[:, :] logLogisticParams, float[:] speiSeries):
+    """
+    Python port of second half of C spei() function.
+
+    Calculates the Standardized Precipitation-Evapotransporation Index
+     from a series of climatic balance (precipitation minus etp). The
+     SPEI is the standardized value of the climatic balance (P-ETP),
+     computed following a Log Logistic probability distribution.
+
+    """
+    cdef int n = dataSeries.shape[0]
+    cdef int i, j
+	# Loop through all seasons defined by seasons
+    for j in range(1, seasons+1):
         # Calculate the standardized values
         for i in range(j-1, n, seasons):
             speiSeries[i] = c_spei.logLogisticCDF(dataSeries[i], &logLogisticParams[j, 0])
